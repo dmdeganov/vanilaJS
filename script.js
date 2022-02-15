193f9db31696528a4a711a6deac0f9eb2ce750d2
Dmitrii Deganov <dmdeganov@yandex.ru>
2022-02-15 21:25:21 +0300

    gitignore

---
+++ script.js
@@ -0,0 +1,13 @@
+const arr = [
+  {
+    id: "news",
+    name: "Новости",
+    path: "news",
+    depth: "1",
+    icon: "cart",
+    count: 6,
+    link: "/news",
+  },
+];
+const json = JSON.stringify(arr);
+console.log(json);
